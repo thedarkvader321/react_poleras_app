cebe437b588a53eb826c185c91150cac6f015227
thedarkvader321 <nicomunoz105@gmail.com>
2023-07-26 20:41:55 -0400

    Proyecto moni

---
+++ src/components/MostrarPolera.jsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import { collection, getDocs, deleteDoc } from 'firebase/firestore'
+import { db } from '../firebase'
+
+
+
+export const MostrarPolera = () => {
+  return (
+    <div>MostrarPolera</div>
+  )
+}
